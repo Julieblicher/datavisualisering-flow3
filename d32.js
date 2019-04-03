d1a1da1b292de0212d8e9dfbc826dc3281db8d0b
Julie Blicher Jensen <julieblicherjensen@gmail.com>
2019-04-03 10:56:45 +0200

    sidste upload

--- d32.js
+++ d32.js
@@ -1,26 +1,29 @@
+// JavaScript Document
+//søjlediagram drenge 
+
 // the dataset, a JavaScript array:
-var dataset = [7824, 2847, 4576, 5673, 2289];
-var element = document.getElementById("d3_svg_bar");
+var datasetdrenge = [7780, 3085, 4812, 5945, 2478];
+var elementdrenge = document.getElementById("d3_svg_bar_drenge");
 
 
 //Width and height of the SVG chart
 var w = 800;
 var h = 600;
 //Create SVG element:
-var svg = d3.select("#d3_svg_bar")
+var svgdrenge = d3.select("#d3_svg_bar_drenge")
  .append("svg") // creates an svg element in the HTML document
  .attr("width", w) // width of the entire svg graphic
  .attr("height", h); // height of the entire svg graphic
 
-svg.selectAll("rect") // This selects all d3 rect elements
+svgdrenge.selectAll("rect") // This selects all d3 rect elements
  /* Other basic d3 shapes:
  circle, line, ellipse, path, polygon, text, polyline
  */
- .data(dataset) // “bind” your data to the type of DOM element you specified in selectAll()
+ .data(datasetdrenge) // “bind” your data to the type of DOM element you specified in selectAll()
  .enter() // the .enter() command matches the selectAll statement with the number of elements in the array/object
  .append("rect") // The .append() command determines which of the SVG basic shapes you’ll use. 
  .attr("x", function(d, i) { // accessing the attribute x (-axis)
- return i * (w / dataset.length);
+ return i * (w / datasetdrenge.length);
  })
  .attr("y", function(d) {
  return h - (d / 15); //Height minus data value: “the top” of each bar in relationship to the top of the SVG!
@@ -29,7 +32,7 @@ svg.selectAll("rect") // This selects all d3 rect elements
  .attr("height", function(d) {
  return d / 15;
  })
-.attr("class", "bars")
+.attr("class", "barsdrenge")
 
 
 ///////////////////////
@@ -37,8 +40,8 @@ svg.selectAll("rect") // This selects all d3 rect elements
 /////////////////////
 
 // 1. Adding the data as text to the bars
-svg.selectAll("text")
- .data(dataset)
+svgdrenge.selectAll("text")
+ .data(datasetdrenge)
  .enter()
  .append("text")
  .text(function(d) {
@@ -46,7 +49,7 @@ svg.selectAll("text")
  })
  .attr("text-anchor", "middle")
  .attr("x", function(d, i) {
- return i * (w / dataset.length) + (w / dataset.length) / 5.5;
+ return i * (w / datasetdrenge.length) + (w / datasetdrenge.length) / 5.5;
  })
  .attr("y", function(d) {
  return h - (d / 15) + 30;
@@ -54,6 +57,3 @@ svg.selectAll("text")
  .attr("font-family", "montserrat", "sans-serif")
  .attr("font-size", "20px")
  .attr("fill", "white");
-
-
-
